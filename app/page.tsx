d950af4aaf8458a3f889c12dc748ef9c16717f5d
aimanjay1 <aimanjay1@gmail.com>
2026-07-27 23:32:54 +0800

    website instruction has created

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,40 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
+        - UBAH DESIGN SUPAYA X NAMPAK MCM AI SLOP
+        <br />
+        <br />
+        - first layer: 2 bahagian: kiri utk codedojo: hack-a-thon bla bla bla + theme + btn register, kanan utk logo
+        <br />
+        - 2nd layer: count down besar.
+        <br />
+        - 3rd layer: tarikh + venue
+        <br />
+        - ubah "500+ hacker" bla bla bla tu ke bwh "everything you need to build"
+        <br />
         <Benefits />
+        - ubah content supaya relate dgn hack-a-thon infos
+        <br />
         <HowItWorks />
+        - buang how it works, ganti design schedule dgn timeline hack-a-thon + design drpd dribbble
+        <br />
+        - letak info ttg sponsors, kasi dia bergerak kanan ke kiri.
+        <br />
+        - prizes dekat bwh, guna design dlm dribble.
+        <br />
         <Tracks />
+        - buang bnde2 yg x berkaitan. seidakan hanya 2 card: kiri utk FAQ, kanan utk Rules.
+        <br />
+        - FAQ guna design style "Questions, answered" punya.
+        <br />
         <Faq />
         <FinalCta />
       </main>
       <Footer />
       <ScrollFab />
+      - ejas2 design dkt footer, bagi x nmpk mcm kosong sgt
+      <br />
+      - dkt design "last call" tu, ubah statement dia supaya relate dgn hack-a-thon + tema hackathon ni.
     </div>
   );
 }
